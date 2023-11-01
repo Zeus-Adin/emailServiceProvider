8891452d1a2fe45dd5be1d728b13a41d4af81618
Zeus-Adin <bollyseaglesway1@gmail.com>
2023-11-01 13:24:10 +0100

    updates

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express')
+const { sendEmail } = require('./functions/mailer')
+const cors = require('cors')
+
+
+// init app & middleware
+const app = express()
+app.use(express.json({ limit: '1mb' }))
+app.use(cors({
+    origin: [process.env.APP_ORIGIN]
+}))
+
+app.post('/api/send/email/:recipient/:username/:emailcontent/:emailtitle', (req, res) => {
+    const { recipient, username, emailcontent, emailtitle } = req.params;
+    console.log(recipient, username, emailcontent, emailtitle)
+    const send = sendEmail(recipient, username, emailcontent, emailtitle);
+    if (send) {
+        res.status(200).json({ sent: true, to: recipient })
+    } else {
+        res.status(500).json({ sent: false, to: recipient })
+    }
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('DB connected at port 3001')
+})
---
+++ functions/mailer.js
@@ -0,0 +1,27 @@
+const nodeMailer = require('nodemailer')
+const { sendBillEmail } = require('./emails')
+
+module.exports = {
+    sendEmail: async (to, username, emainContent, emailTitle) => {
+        const emailBody = sendBillEmail(username, emainContent, emailTitle)
+        const transporter = nodeMailer.createTransport({
+            host: 'mail.bitcoinpro24.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'team@bitcoinpro24.com',
+                pass: 'O*p@=i7kl-yE'
+            }
+        })
+        transporter.sendMail({
+            from: 'team@bitcoinpro24.com',
+            to: to,
+            subject: emailTitle,
+            html: emailBody
+        }).then(res => {
+            return true
+        })
+            .catch(e => false)
+
+    }
+}
